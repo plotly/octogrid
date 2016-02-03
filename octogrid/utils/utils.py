d1557a667f46564e91372c67562151cc439068a1
Pravendra Singh <hackpravj@gmail.com>
2016-02-03 11:09:30 +0530

    Fix installation issue caused by wrong file path
    > While filling 'install_requires', the path for 'requirements.txt' was
    > wrong, resulting in erron on install.

--- octogrid/utils/utils.py
+++ octogrid/utils/utils.py
@@ -30,20 +30,3 @@ def username_to_file(username):
 	"""
 
 	return '{0}.gml'.format(username)
-
-
-def build_dependecies():
-	"""
-	Returns a list representing all the required packages
-	"""
-
-	package_list = []
-
-	with open('requirements.txt', 'r') as f:
-		lines = f.readlines()
-
-	for line in lines:
-		name, version = line.split('==')
-		package_list.append('{0}>={1}'.format(name, version))
-
-	return package_list
--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 import os
-from octogrid.utils import utils
 from octogrid import __version__
 from setuptools import setup
 
 
-# read content from utility files
 def read(filename):
+    """
+    Read content from utility files
+    """
+
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+def build_dependecies():
+    """
+    Returns a list representing all the required packages
+    """
+
+    package_list = []
+
+    with open('requirements.txt', 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        name, version = line.split('==')
+        package_list.append('{0}>={1}'.format(name, version))
+
+    return package_list
+
 
 setup(
     name='octogrid',
@@ -18,19 +38,19 @@ setup(
     keywords = 'github network graph plotly plot chart visualization',
     url = 'https://github.com/pravj/octogrid',
     packages=['octogrid', 'octogrid.parser', 'octogrid.generator', 'octogrid.auth', 'octogrid.builder', 'octogrid.publisher', 'octogrid.store', 'octogrid.utils'],
-    install_requires=utils.build_dependecies(),
+    install_requires=build_dependecies(),
     long_description=read('README.rst'),
     entry_points={
         'console_scripts': ['octogrid = octogrid.octogrid:main']
     },
     classifiers = [
-      'Development Status :: 5 - Production/Stable',
-      'Environment :: Console',
-      'Operating System :: OS Independent',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: MIT License',
-      'Natural Language :: English',
-      'Programming Language :: Python :: 2.6',
-      'Programming Language :: Python :: 2.7'
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
     ]
 )
